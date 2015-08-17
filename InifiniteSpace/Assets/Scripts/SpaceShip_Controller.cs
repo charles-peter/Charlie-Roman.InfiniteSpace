c286078ca48d2a546b68d25834b865f7b21e734e
Roman Taran <Roman.v.taran@gmail.com>
2015-08-17 14:16:30 -0400

    Added the space ship model

---
+++ InifiniteSpace/Assets/Scripts/SpaceShip_Controller.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpaceShip_Controller : MonoBehaviour {
+
+
+    Rigidbody m_rigidbody;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        float fVertical = Input.GetAxis("Vertical");
+        float fHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 move = new Vector3(fHorizontal,0.0f, fVertical);
+
+        m_rigidbody.velocity = move*25;
+	}
+}
