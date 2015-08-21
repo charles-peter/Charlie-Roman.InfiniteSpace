fedd144156ace171c44bdef7c59b9c8d0227b383
charles-peter <charliepeter2011@yahoo.com>
2015-08-21 14:04:00 -0400

    I did Shit today.
    Removed some unused textures from a model we weren't using. Created a
    projectile prefab, with vfx. The player can now shoot projectiles that
    destroy themselves after a lifetime.

--- InifiniteSpace/Assets/Scripts/Projectile.cs
+++ InifiniteSpace/Assets/Scripts/Projectile.cs
@@ -4,11 +4,23 @@ using System.Collections;
 public class Projectile : MonoBehaviour 
 {
 	public float speed;
+	public float LifeTime;
 	Rigidbody m_rigidbody;
+	float life = 0.0f;
+
 
 	void Start()
 	{
 		m_rigidbody = GetComponent<Rigidbody>();
+		LifeTime += Time.time;
+
+	}
+
+	void Update()
+	{
+		life = Time.time;
+		if (life > LifeTime)
+			Destroy(gameObject);
 	}
 
 	void FixedUpdate()
--- InifiniteSpace/Assets/Scripts/SpaceShip_Controller.cs
+++ InifiniteSpace/Assets/Scripts/SpaceShip_Controller.cs
@@ -9,13 +9,30 @@ public class SpaceShip_Controller : MonoBehaviour {
     Transform m_Transform;
     public float Tilt;
     public float Speed;
+
+	//All the Variables needed for firing projectiles. 
+	public GameObject projectile;
+	public Transform projSpawn;
+	public float fireRate;
+	float nextFire = 0.0f;
+
 	// Use this for initialization
 	void Start () 
     {
         m_rigidbody = GetComponent<Rigidbody>();
         m_Transform = GetComponent<Transform>();
 	}
-	
+
+
+	void Update()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) 
+		{
+			nextFire = Time.time + fireRate;
+			Instantiate(projectile, projSpawn.position, projSpawn.rotation);
+		}
+	}
+
 	// Update is called once per frame
 	void FixedUpdate () 
     {
