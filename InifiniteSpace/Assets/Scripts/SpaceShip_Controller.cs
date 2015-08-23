880b1754a334f92140074cb316c96412cbe9cb50
Roman Taran <Roman.v.taran@gmail.com>
2015-08-23 18:33:58 -0400

    Ship Bank on Turn
    The  Player ship banks on turns

--- InifiniteSpace/Assets/Scripts/SpaceShip_Controller.cs
+++ InifiniteSpace/Assets/Scripts/SpaceShip_Controller.cs
@@ -5,15 +5,19 @@ public class SpaceShip_Controller : MonoBehaviour {
 
 
     Rigidbody m_rigidbody;
-    GameObject playerModel;
+    public GameObject playerModel;
     Transform m_Transform;
     public float Tilt;
     public float Speed;
-	// Use this for initialization
+
+   
+
 	void Start () 
     {
         m_rigidbody = GetComponent<Rigidbody>();
         m_Transform = GetComponent<Transform>();
+        
+        
 	}
 	
 	// Update is called once per frame
@@ -22,9 +26,19 @@ public class SpaceShip_Controller : MonoBehaviour {
         float fVertical = Input.GetAxis("Vertical");
         float fHorizontal = Input.GetAxis("Horizontal");
 
-        Vector3 move = new Vector3(0.0f,0.0f, fVertical);
+        //Vector3 move = new Vector3(0.0f,0.0f, fVertical);
 
-        m_rigidbody.velocity = m_Transform.forward *Speed *fVertical;
-        m_Transform.Rotate(0.0f, fHorizontal * Tilt, 0.0f);
+		m_Transform.Rotate(0f,fHorizontal,0f);
+		m_rigidbody.velocity = m_Transform.forward *Speed *fVertical;
+
+
+		playerModel.transform.localRotation = Quaternion.Euler(0f,0f,fHorizontal * -Tilt);
+
+
+
+        
+
+
+        
 	}
 }
