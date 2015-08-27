b5a40c83fe3c6b9494ff852b4486fee083d7800c
charles-peter <charliepeter2011@yahoo.com>
2015-08-27 17:41:38 -0400

    Enemies can now follow the player.
    Enemies detect the play and can now follow them. I deleted the other
    enemy that was just placed in the scene. Moved the enemy spawner back 50
    units and decreased the scale of enemy ships to 10 instead of 15.

--- InifiniteSpace/Assets/Scripts/Enemy_Controller.cs
+++ InifiniteSpace/Assets/Scripts/Enemy_Controller.cs
@@ -8,10 +8,14 @@ public class Enemy_Controller : MonoBehaviour
 
 	GameObject target;
 
+	//General Enemy Attributes
 	public int m_heatlh;
 	public float m_speed;
 	public float m_turnRate;
-	public int m_RadarRange;
+	public bool m_BaseBehavior;
+
+	//Ai Behavior stuff.
+	Vector3 m_ToTarget;
 
 	void Start()
 	{
@@ -29,24 +33,39 @@ public class Enemy_Controller : MonoBehaviour
 
 	void FixedUpdate()
 	{
-		/*if(target != null && target.tag == "Player") 
+		if (m_BaseBehavior == true) 
 		{
-			//m_rigidbody.velocity = Vector3(0f, 0f, 0f);
-			//m_rigidbody.velocity = m_transform.forward * m_speed;
-		} 
-		else*/
-		{
-			m_transform.Rotate(0f, m_turnRate, 0f);
-			m_rigidbody.velocity = m_transform.forward * m_speed;
+			if (target != null && target.tag == "Player") 
+			{
+				m_ToTarget = target.GetComponent<SpaceShip_Controller>().transform.position - m_transform.position;
+				// The step size is equal to speed times frame time.
+				var step = m_turnRate * Time.deltaTime;
+				var newDir = Vector3.RotateTowards(m_transform.forward, m_ToTarget, step, 0.0f);
+				Debug.DrawRay(transform.position, newDir, Color.red);
+				// Move our position a step closer to the target.
+				m_transform.rotation = Quaternion.LookRotation(newDir);
+				
+				m_rigidbody.velocity = m_transform.forward * m_speed;
+			} 
+			else 
+			{
+				//If there is no target it does a base patrol;
+				m_transform.Rotate (0f, m_turnRate, 0f);
+				m_rigidbody.velocity = m_transform.forward * m_speed;
+			}
 		}
 	}
-
+	
 	void OnTriggerEnter(Collider other)
 	{
-		if(other.tag == "BaseProjectile") 
+		if (other.tag == "BaseProjectile") 
 		{
-			m_heatlh -= other.GetComponent<Projectile>().Damage;
-		}
+			m_heatlh -= other.GetComponent<Projectile> ().Damage;
+		} 
+//		else if (other.tag == "Player") 
+//		{
+//			Destroy(gameObject);
+//		}
 	}
 	
 	public GameObject Target {
