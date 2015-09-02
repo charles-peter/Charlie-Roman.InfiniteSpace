e027aa5e131d58d9922fc3792664e749aa3193d1
Roman Taran <Roman.v.taran@gmail.com>
2015-09-02 15:12:02 -0400

    Enemy Radar-Missile Collision
    Enemy Radar Logic and Missile collison

--- InifiniteSpace/Assets/Scripts/Enemy_Controller.cs
+++ InifiniteSpace/Assets/Scripts/Enemy_Controller.cs
@@ -14,13 +14,13 @@ public class Enemy_Controller : MonoBehaviour, IDamageable<int>
 	Rigidbody m_rigidbody;
 	Transform m_transform;
 
-	GameObject target;
+	Enemy_Radar myRadar;
 
 	//General Enemy Attributes
 	public int m_heatlh;
 	public float m_speed;
 	public float m_turnRate;
-	public bool m_BaseBehavior;
+	public bool m_BaseBehavior = false;
 
 	//Ai Behavior stuff.
 	Vector3 m_ToTarget;
@@ -29,6 +29,7 @@ public class Enemy_Controller : MonoBehaviour, IDamageable<int>
 	{
 		m_rigidbody = GetComponent<Rigidbody>();
 		m_transform = GetComponent<Transform>();
+		myRadar = GetComponentInChildren<Enemy_Radar>();
 	}
 
 	void Update()
@@ -43,9 +44,10 @@ public class Enemy_Controller : MonoBehaviour, IDamageable<int>
 	{
 		if (m_BaseBehavior == true) 
 		{
-			if (target != null && target.tag == "Player") 
+			//if (target != null && target.tag == "Player")
+			if(myRadar.GetTarget())
 			{
-				m_ToTarget = target.GetComponent<SpaceShip_Controller>().transform.position - m_transform.position;
+				m_ToTarget = myRadar.GetTarget().transform.position - m_transform.position;
 				// The step size is equal to speed times frame time.
 				var step = m_turnRate * Time.deltaTime;
 				var newDir = Vector3.RotateTowards(m_transform.forward, m_ToTarget, step, 0.0f);
@@ -55,12 +57,13 @@ public class Enemy_Controller : MonoBehaviour, IDamageable<int>
 				
 				m_rigidbody.velocity = m_transform.forward * m_speed;
 			} 
-			else 
-			{
-				//If there is no target it does a base patrol;
-				m_transform.Rotate (0f, m_turnRate, 0f);
-				m_rigidbody.velocity = m_transform.forward * m_speed;
-			}
+
+		}
+		else 
+		{
+			//If there is no target it does a base patrol;
+			m_transform.Rotate (0f, m_turnRate, 0f);
+			m_rigidbody.velocity = m_transform.forward * m_speed;
 		}
 	}
 	
@@ -77,13 +80,17 @@ public class Enemy_Controller : MonoBehaviour, IDamageable<int>
 //			Destroy(gameObject);
 //		}
 	}
-	
-	public GameObject Target {
-		get {
-			return target;
-		}
-		set {
-			target = value;
-		}
+
+	public void TargetAcquired(bool b)
+	{
+		m_BaseBehavior = b;
 	}
+//	public GameObject Target {
+//		get {
+//			return target;
+//		}
+//		set {
+//			target = value;
+//		}
+//	}
 }
--- InifiniteSpace/Assets/Scripts/Missile_Behavior.cs
+++ InifiniteSpace/Assets/Scripts/Missile_Behavior.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Missile_Behavior : MonoBehaviour {
 
@@ -16,6 +17,9 @@ public class Missile_Behavior : MonoBehaviour {
 	Rigidbody m_RigidBody;
 	Transform m_Transform;
 	ParticleSystem exhaust;
+	public GameObject explosionPrefab;
+
+
 	public bool InFlight {
 		get {
 			return inFlight;
@@ -53,6 +57,18 @@ public class Missile_Behavior : MonoBehaviour {
 		m_RigidBody.isKinematic = true;
 		m_Transform = GetComponent<Transform>();
 		exhaust = GetComponentInChildren<ParticleSystem>();
+
+//		ParticleSystem[] temp = GetComponentsInChildren<ParticleSystem>();
+//		Debug.Log (temp.Length);
+//		foreach(ParticleSystem p in temp)
+//		{
+//			if(p.name == "ShockFlame")
+//			{
+//				explosion = p;
+//				break;
+//			}
+//		}
+
 	}
 
 	void Update()
@@ -73,6 +89,19 @@ public class Missile_Behavior : MonoBehaviour {
 			m_Transform.rotation = Quaternion.Slerp(m_Transform.rotation, Quaternion.LookRotation(target.transform.position- m_Transform.position),TrackAngle * Time.deltaTime);
 			m_RigidBody.velocity = m_Transform.forward * Speed;
 			m_RigidBody.AddForce(m_Transform.forward * Speed,ForceMode.Acceleration);
+		}
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "BaseEnemy")
+		{
+			IDamageable<int> d = other.gameObject.GetComponent<IDamageable<int>>();
+			d.TakeDamage(Damage);
+
+			Destroy(gameObject);
+			//explosion.Play();
+			Instantiate(explosionPrefab,m_Transform.position, m_Transform.rotation);
 		}
 	}
 }
