46229db36bbc23be30690b0bce18e21b4a30e0a9
charles-peter <charliepeter2011@yahoo.com>
2015-09-02 15:51:54 -0400

    fuck it

--- InifiniteSpace/Assets/Scripts/Enemy_Controller.cs
+++ InifiniteSpace/Assets/Scripts/Enemy_Controller.cs
@@ -22,7 +22,6 @@ public class Enemy_Controller : MonoBehaviour, IDamageable<int>
 	public float m_speed;
 	public float m_turnRate;
 	public float m_fireRange;
-	public bool m_BaseBehavior;
 	public bool m_BaseBehavior = false;
 
 
